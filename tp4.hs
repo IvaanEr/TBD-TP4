eeb06c327ca13dcd975c5480ea28676c91ac324d
IvaanEr <ivan_ernandorena@hotmail.com>
2016-11-11 13:23:43 -0300

    closeSet

--- tp4.hs
+++ tp4.hs
@@ -6,6 +6,9 @@ import Control.Monad
 -- showwwwww
 -- toList  $ Data.Set.map (\(x,y) -> (concat $ toList  x,concat $ toList  y)) ans
  
+r = fromList ["A","B","C","D"]
+f = fromList [(fromList ["A"],fromList ["B"]),(fromList ["C","B"],fromList ["A"]),(fromList ["B"],fromList["A","D"])]
+
 powerset :: Ord a => Set a -> Set (Set a)
 powerset = fromList . fmap fromList . listPowerset . toList
  
@@ -31,12 +34,25 @@ concatSet set | set == empty = empty
                where f []     = empty
                      f (x:xs) = union x (f xs)
 
--- regla de aumento	
+-- regla de aumento 
 --alfa -> beta => alfa gama => beta gama tq gama \in 
 
-
 closeAum :: Set String -> Set (Set String,Set String) -> Set (Set String,Set String)
-closeAum r f = let pr = delete empty $ powerset r
-				        
-               in  Data.Set.map (\rr -> Data.Set.map (\(x,y)-> (union x rr, union y rr)) f ) pr
+closeAum r f = let pr = delete empty $ powerset r                       
+               in unions $ toList $ Data.Set.map (\rr -> Data.Set.map (\(x,y)-> (union x rr, union y rr)) f ) pr
 
+f' = fromList [(fromList ["A"],fromList ["B"]),(fromList ["B"],fromList ["C"])]
+
+closeTrans :: Set (Set String,Set String) -> Set (Set String,Set String)
+closeTrans f = unions $ toList $ Data.Set.map (\(a1,b1)-> Data.Set.map (\(a2,b2)-> if b1 == a2 then (a1,b2) else (a2,b2)) f) f
+
+
+closeSet :: Set String -> Set (Set String,Set String) -> Set (Set String,Set String)
+closeSet r f = let step1 = closeRef r f
+                   step2 = closeAum r f
+                   step3 = closeTrans f
+               in union (union step1 step2) step3
+
+closeSetPrint r f = toList  $ Data.Set.map (\(x,y) -> (concat $ toList  x,concat $ toList  y)) $ closeSet r f
+
+closeSetCard r f = size $ closeSet r f
