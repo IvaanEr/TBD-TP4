5f905fed48e661b94ca50d4a5d59b77b40681f65
IvaanEr <ivan_ernandorena@hotmail.com>
2016-11-11 21:59:16 -0300

    Corregido + IO

--- tp4.hs
+++ tp4.hs
@@ -1,13 +1,16 @@
-module TP4 where 
+-- module TP4 where 
 
 import Data.Set
 import Control.Monad
 
 -- showwwwww
--- toList  $ Data.Set.map (\(x,y) -> (concat $ toList  x,concat $ toList  y)) ans
- 
-r = fromList ["A","B","C","D"]
-f = fromList [(fromList ["A"],fromList ["B"]),(fromList ["C","B"],fromList ["A"]),(fromList ["B"],fromList["A","D"])]
+niceShow f = toList  $ Data.Set.map (\(x,y) -> (concat $ toList  x,concat $ toList  y)) f
+
+r1 = fromList ["A","B","C","D"]
+f1 = fromList [(fromList ["A"],fromList ["B"]),(fromList ["C","B"],fromList ["A"]),(fromList ["B"],fromList["A","D"])]
+
+r2 = fromList ["A","B","C","D","E", "F"]
+f2 = fromList [(fromList ["A","B"],fromList ["C"]),(fromList ["B","D"],fromList ["E","F"])]
 
 powerset :: Ord a => Set a -> Set (Set a)
 powerset = fromList . fmap fromList . listPowerset . toList
@@ -41,7 +44,7 @@ closeAum :: Set String -> Set (Set String,Set String) -> Set (Set String,Set Str
 closeAum r f = let pr = delete empty $ powerset r                       
                in unions $ toList $ Data.Set.map (\rr -> Data.Set.map (\(x,y)-> (union x rr, union y rr)) f ) pr
 
-f' = fromList [(fromList ["A"],fromList ["B"]),(fromList ["B"],fromList ["C"])]
+f' = fromList [(fromList ["A"],fromList ["B"]),(fromList ["B"],fromList ["C"]),(fromList ["C"],fromList ["D"])]
 
 closeTrans :: Set (Set String,Set String) -> Set (Set String,Set String)
 closeTrans f = unions $ toList $ Data.Set.map (\(a1,b1)-> Data.Set.map (\(a2,b2)-> if b1 == a2 then (a1,b2) else (a2,b2)) f) f
@@ -49,10 +52,23 @@ closeTrans f = unions $ toList $ Data.Set.map (\(a1,b1)-> Data.Set.map (\(a2,b2)
 
 closeSet :: Set String -> Set (Set String,Set String) -> Set (Set String,Set String)
 closeSet r f = let step1 = closeRef r f
-                   step2 = closeAum r f
-                   step3 = closeTrans f
-               in union (union step1 step2) step3
+               in closeSet' r step1
+
+closeSet' :: Set String -> Set (Set String,Set String) -> Set (Set String,Set String)
+closeSet' r f = let step2 = closeAum r f
+                    step3 = closeTrans step2
+                in if step3 == f then step3 else closeSet' r step3
+
 
 closeSetPrint r f = toList  $ Data.Set.map (\(x,y) -> (concat $ toList  x,concat $ toList  y)) $ closeSet r f
 
-closeSetCard r f = size $ closeSet r f
+closeSetCard r f = size $  closeSet r f
+
+prueba :: IO ()
+prueba = do putStrLn "Set1 -> R: {A, B, C, D} F: {{A}→{B}, {B}→{A,D}, {B,C}→{A}}"
+            putStr "Cardinalidad:"
+            putStrLn $ show $ closeSetCard r1 f1
+            putStrLn "Set2 -> R: {A, B, C, D, E, F} F: {AB→C, BD→EF}"
+            putStr "Cardinalidad:"
+            putStrLn $ show $ closeSetCard r2 f2
+
